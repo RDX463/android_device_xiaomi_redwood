9afb2a26b5f2f72a8d89bfc5d9fe7a79ef634b4d
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-12-13 20:52:11 +0300

    redwood: Link dolby blobs against v33 libstagefright_foundation
     * fixes crashes and absurdly high CPU usage while using dolby
    
    Co-authored-by: Woomymy <woomy@woomy.be>

--- extract-files.sh
+++ extract-files.sh
@@ -89,6 +89,9 @@ function blob_fixup() {
         vendor/etc/init/vendor.qti.media.c2@1.0-service.rc)
             sed -i "s|writepid /dev/cpuset/foreground/tasks|task_profiles ProcessCapacityHigh|g" "${2}"
              ;;
+        vendor/lib/libstagefright_soft_ac4dec.so | vendor/lib/libstagefright_soft_ddpdec.so | vendor/lib/libstagefrightdolby.so | vendor/lib64/libdlbdsservice.so | vendor/lib64/libstagefright_soft_ac4dec.so | vendor/lib64/libstagefright_soft_ddpdec.so | vendor/lib64/libstagefrightdolby.so)
+            grep -q "libstagefright_foundation-v33.so" "${2}" || "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
     esac
 }
 
